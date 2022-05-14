f013daa97278cf6376ead1235385c0ac4f26010f
Jonathan Jones <97317524+JonJones98@users.noreply.github.com>
2022-05-14 03:52:45 -0400

    Saving Updates

--- flask_app/controllers/qrcode.py
+++ flask_app/controllers/qrcode.py
@@ -1,3 +1,5 @@
+import shutil
+import requests
 from threading import local
 from traceback import format_list
 from flask import render_template,redirect,request, session
@@ -7,6 +9,7 @@ from PIL import Image
 import urllib
 from pathlib import Path
 import os
+import base64
 
 
 @app.route("/")
@@ -29,23 +32,35 @@ def validate():
     img =qrcode.make(qr_url)
     type(img)
     session['file name']=(qr_name,qr_format)
-    #path_to_download_folder = (os.path.join(Path.home(), "Desktop"))
+    #path_to_download_folder = (os.path.join(Path.home(), "Downloads"))
     #session['file location']="%s%c"(path_to_download_folder,session['file name'])
     session['file location']= "qr_code/%s%s"%(qr_name,qr_format)
     #img.save(path_to_download_folder+session['file name'])
     img.save(session['file location'])
+    #prepre=Image.SAVE(session['file location'])
+    preview=Image.open(session['file location'])
+    preview.show()
+    pre=session['file location']
+    #preview.tobytes(encoder_name=(os.path.join(Path.home(), "Desktop")))
     return redirect("/qrcodedownload")
     #return render_template("validateQRCreation.html",submission=data)
 
 @app.route("/qrcodedownload")
 def download():
-    qr_Name = session['file name']
-    qrImage = session['file location']
+    qr_Name = str(session['file name'])
+    qrImage = (session['file location'])
+    #image = open(qrImage, 'rb') #open binary file in read mode
+    #image_read = image.read()
+    #image_64_encode = base64.b64encode(image_read)
+    url="qr_code"
+    #res = request.get(url, stream=True)
+    #with open(os.path.join(Path.home(), "Downloads",str(session["filename"])),'wb')as f:
+     #   shutil.copyfileobj(qrImage, f)
     return redirect("/downloads")
 
 @app.route("/downloads")
 def source():
-    preview=Image.open(session['file location'])
-    preview.show()
+    #preview=Image.open(session['file location'])
+    #preview.show()
     #return render_template("succesfulQRCreation.html",preview=preview)
     return redirect("/")
