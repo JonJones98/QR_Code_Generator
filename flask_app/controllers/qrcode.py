4abb790c3e035fa6a54dc6839ddf6c8f7b4102ed
Jonathan Jones <97317524+JonJones98@users.noreply.github.com>
2022-05-25 07:26:21 -0400

    Saving Updates 052522

--- flask_app/controllers/qrcode.py
+++ flask_app/controllers/qrcode.py
@@ -1,3 +1,4 @@
+
 import shutil
 import requests
 from threading import local
@@ -10,6 +11,7 @@ import urllib
 from pathlib import Path
 import os
 import base64
+import requests
 
 
 @app.route("/")
@@ -29,38 +31,16 @@ def validate():
     qr_url=str(session['url'])
     qr_name=str(session["filename"])
     qr_format=str(session["formattype"])
-    img =qrcode.make(qr_url)
-    type(img)
-    session['file name']=(qr_name,qr_format)
-    #path_to_download_folder = (os.path.join(Path.home(), "Downloads"))
-    #session['file location']="%s%c"(path_to_download_folder,session['file name'])
-    session['file location']= "qr_code/%s%s"%(qr_name,qr_format)
-    #img.save(path_to_download_folder+session['file name'])
-    img.save(session['file location'])
-    #prepre=Image.SAVE(session['file location'])
-    preview=Image.open(session['file location'])
-    preview.show()
-    pre=session['file location']
-    #preview.tobytes(encoder_name=(os.path.join(Path.home(), "Desktop")))
+    session['file name']= "%s%s"%(qr_name,qr_format)
     return redirect("/qrcodedownload")
-    #return render_template("validateQRCreation.html",submission=data)
 
 @app.route("/qrcodedownload")
 def download():
-    qr_Name = str(session['file name'])
-    qrImage = (session['file location'])
-    #image = open(qrImage, 'rb') #open binary file in read mode
-    #image_read = image.read()
-    #image_64_encode = base64.b64encode(image_read)
-    url="qr_code"
-    #res = request.get(url, stream=True)
-    #with open(os.path.join(Path.home(), "Downloads",str(session["filename"])),'wb')as f:
-     #   shutil.copyfileobj(qrImage, f)
-    return redirect("/downloads")
-
-@app.route("/downloads")
-def source():
-    #preview=Image.open(session['file location'])
-    #preview.show()
-    #return render_template("succesfulQRCreation.html",preview=preview)
+    img =qrcode.make(str(session['url'])) 
+    type(img)
+    path_to_download_folder = (os.path.join(Path.home(), "Downloads",session['file location']))
+    img.save(path_to_download_folder )
+    session.clear()
+    print("Download Complete")
+    print(session)
     return redirect("/")
