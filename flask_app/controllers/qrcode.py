31fd0c46a79d3401a4ecc93857e8be99d037b6a9
Jonathan Jones <97317524+JonJones98@users.noreply.github.com>
2022-07-22 12:06:14 -0400

    Updating look

--- flask_app/controllers/qrcode.py
+++ flask_app/controllers/qrcode.py
@@ -1,19 +1,9 @@
 
-import shutil
-from turtle import delay
-import requests
-from threading import local
-from traceback import format_list
 from flask import render_template,redirect,request, session
 from flask_app import app
 import qrcode
-from PIL import Image
-import urllib
-from pathlib import Path
 import os
-import base64
-import requests
-import time
+from flask_app.models.qrcode import Validate
 
 
 @app.route("/")
@@ -22,15 +12,19 @@ def index():
 
 @app.route("/qrcodecreation")
 def home():
-    format_list=[".tif",".tiff",".bmp",".jpg",".jpeg",".gif",".png",".eps",".raw",".cr2",".nef",".orf",".sr2",".pdf",".img"]
-    return render_template("home_page.html",formats=format_list)
+    format_list=["Select Format",".tif",".tiff",".bmp",".jpg",".jpeg",".gif",".png",".eps",".raw",".cr2",".nef",".orf",".sr2",".pdf",".img"]
+    return render_template("home_page.html",formats=format_list, urlvalue=session['url'],namevalue=session['name'],formatvalue=session['formattype'])
 
 @app.route("/qrcodevalidate", methods=["POST"])
 def validate():
-    session['url']=request.form["url"]
-    session['name']=request.form["filename"]
-    session['formattype']=request.form["formattype"]
-    qr_url=str(session['url'])
+    try:
+        session['url']=request.form["url"]
+        session['name']=request.form["filename"]
+        session['formattype']=request.form["formattype"]
+        if not Validate.validate_info(request.form):
+            return redirect ("/")
+    except:
+        return redirect("/qrcodecreation")
     qr_name=str(session["name"])
     qr_format=str(session["formattype"])
     session['file name']= "%s%s"%(qr_name,qr_format)
@@ -51,17 +45,8 @@ def loading():
         os.remove('flask_app/static/image/'+session['file name'])
     except:
         print("folder empty")
-    session.clear
-    return redirect("/qrcodecreation")
-
-#@app.route("/qrcodedownload1")
-#def download1():
-    img =qrcode.make(str(session['url'])) 
-    type(img)
-    path_to_download_folder = (os.path.join(Path.home(), "Downloads",session['file name']))
-    img.save(path_to_download_folder )
     session.clear()
-    print("Download Complete")
-    print(session)
-    return render_template("succesfulQRCreation.html")
-
+    session['url']=""
+    session['name']=""
+    session['formattype']=""
+    return redirect("/qrcodecreation")
--- flask_app/models/qrcode.py
+++ flask_app/models/qrcode.py
@@ -1,5 +1,19 @@
 from unittest import result
 from flask_app.config.mysqlconnection import connectToMySQL
-
+from flask import flash
+class Validate:
+    @staticmethod
+    def validate_info(QRinfo):
+        is_valid = True # we assume this is true
+        if len(QRinfo['url']) < 1:
+            flash("*URL field is missing")
+            is_valid = False
+        if len(QRinfo['filename']) < 1:
+            flash("*Filename is missing")
+            is_valid = False
+        if (QRinfo['formattype'])in "Select Format":
+            flash("*Select Format type")
+            is_valid = False
+        return is_valid
 
 
