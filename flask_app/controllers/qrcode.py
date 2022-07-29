7491f752499429cf66861730a73df4aa4d31d2de
Ubuntu <ubuntu@ip-172-31-86-141.ec2.internal>
2022-07-29 13:09:17 +0000

    Update Routes

--- flask_app/controllers/qrcode.py
+++ flask_app/controllers/qrcode.py
@@ -11,12 +11,12 @@ def index():
     session['url']=""
     session['name']=""
     session['formattype']=""
-    return redirect("/qrcodecreation")
+    return redirect("/qr/create")
 @app.route("/error")
 def val_error():
-    return redirect("/qrcodecreation")
+    return redirect("/qr/create")
 
-@app.route("/qrcodecreation")
+@app.route("/qr/create")
 def home():
     try:
         format_list=["Select Format",".tif",".tiff",".bmp",".jpg",".jpeg",".gif",".png",".eps",".raw",".cr2",".nef",".orf",".sr2",".pdf",".img"]
@@ -24,8 +24,8 @@ def home():
     except:
         return redirect("/")
 
-@app.route("/qrcodevalidate", methods=["POST"])
-def validate():
+@app.route("/qr/constructing", methods=["POST"])
+def validate_and_constructing():
     try:
         session['url']=request.form["url"]
         session['name']=request.form["filename"]
@@ -33,25 +33,25 @@ def validate():
         if not Validate.validate_info(request.form):
             return redirect ("/error")
     except:
-        return redirect("/qrcodecreation")
+        return redirect("/qr/create")
     qr_name=str(session["name"])
     qr_format=str(session["formattype"])
     session['file name']= "%s%s"%(qr_name,qr_format)
     return render_template("index.html")
-@app.route("/qrcodeproduction")
-def downloadpage():
+@app.route("/qr/complete")
+def complete():
     try:
-        img =qrcode.make(str(session['url'])) 
+        img=qrcode.make(str(session['url'])) 
         type(img)
         file_name=session['file name']
         img.save('flask_app/static/image/'+file_name)
         return render_template("succesfulQRCreation.html")
     except:
-        return redirect("/qrcodedownload")
-@app.route("/qrcodedownload")
+        return redirect("/qr/")
+@app.route("/qr/download")
 def download():
     return render_template("download_page.html",QR_name=session['file name'])
-@app.route("/qrcodereset")
+@app.route("/qr/reset")
 def loading():
     try:
         os.remove('flask_app/static/image/'+session['file name'])
